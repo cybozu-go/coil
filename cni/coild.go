0db73c85db422ad6466d6fd9ae6ed92672114adb
ymmt <ymmt@cybozu.com>
2018-10-16 17:21:06 +0000

    [coil] implement delete command

--- cni/coild.go
+++ cni/coild.go
@@ -49,5 +49,22 @@ func getIPFromCoild(coild *url.URL, podNS, podName string) (ip, gw net.IP, err e
 }
 
 func returnIPToCoild(coild *url.URL, podNS, podName string) error {
+	u := *coild
+	u.Path = path.Join(u.Path, "/ip", podNS, podName)
+
+	req, err := http.NewRequest(http.MethodDelete, u.String(), nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNotFound {
+		return fmt.Errorf("coild returns %d", resp.StatusCode)
+	}
+
 	return nil
 }
--- cni/del.go
+++ cni/del.go
@@ -1,13 +1,48 @@
 package cni
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/url"
 
 	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/plugins/pkg/ip"
+	"github.com/containernetworking/plugins/pkg/ns"
 )
 
 // Del free the IP address allocated for a container.
 func Del(args *skel.CmdArgs) error {
-	// TODO: implement
-	return fmt.Errorf("not implemented")
+	conf := new(PluginConf)
+	err := json.Unmarshal(args.StdinData, conf)
+	if err != nil {
+		return err
+	}
+	coildURL, err := url.Parse(conf.CoildURL)
+	if err != nil {
+		return err
+	}
+
+	kv := parseArgs(args.Args)
+	podNS, podName, err := getPodInfo(kv)
+	if err != nil {
+		return err
+	}
+
+	err = returnIPToCoild(coildURL, podNS, podName)
+	if err != nil {
+		return err
+	}
+
+	// Kubernetes sends multiple DEL for dead containers.
+	// See: https://github.com/kubernetes/kubernetes/issues/44100
+	if args.Netns == "" {
+		return nil
+	}
+
+	return ns.WithNetNSPath(args.Netns, func(_ ns.NetNS) error {
+		err := ip.DelLinkByName(args.IfName)
+		if err == nil || err == ip.ErrLinkNotFound {
+			return nil
+		}
+		return err
+	})
 }
