6fd26ebbfab7b50ceafc2a3a4386c6fe485ca343
mitz <mitz@cybozu.co.jp>
2019-05-10 18:56:08 +0900

    Fix gofmt

--- coild/main_test.go
+++ coild/main_test.go
@@ -7,11 +7,11 @@ import (
 	"github.com/cybozu-go/coil/test"
 )
 
-
 const (
 	clientPort = "12379"
-	peerPort = "12380"
+	peerPort   = "12380"
 )
+
 func TestMain(m *testing.M) {
 	os.Exit(test.TestEtcdRun(m, clientPort, peerPort))
 }
