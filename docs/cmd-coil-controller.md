36192fe574ea6015da9bf3c3281bd05d1b3c1ef2
ymmt <ymmt2005@gmail.com>
2020-09-01 04:15:10 +0000

    cmd/coil-controller: implemented

---
+++ docs/cmd-coil-controller.md
@@ -0,0 +1,34 @@
+coil-controller
+===============
+
+`coil-controller` is a Kubernetes controller for Coil custom resources.
+It is intended to be run as a Pod in `kube-system` namespace.
+
+## AddressPool and AddressBlock
+
+`coil-controller` has an in-memory database of address pools and
+address blocks to allocate address blocks quickly.
+
+## BlockRequest
+
+`coil-controller` watches newly created block requests and curve out
+address blocks from the requested pool.
+
+## Egress
+
+`coil-controller` creates Deployment and Service for each Egress.
+
+## Garbage collection
+
+`coil-controller` periodically checks orphaned address blocks and deletes them.
+
+## Command-line flags
+
+```
+Flags:
+      --gc-interval duration   garbage collection interval (default 1h0m0s)
+      --health-addr string     bind address of health/readiness probes (default ":9387")
+  -h, --help                   help for coil-controller
+      --metrics-addr string    bind address of metrics endpoint (default ":9386")
+  -v, --version                version for coil-controller
+```
---
+++ v2/cmd/coil-controller/sub/run.go
@@ -0,0 +1,96 @@
+package sub
+
+import (
+	"time"
+
+	coilv2 "github.com/cybozu-go/coil/v2/api/v2"
+	"github.com/cybozu-go/coil/v2/controllers"
+	"github.com/cybozu-go/coil/v2/pkg/ipam"
+	"github.com/cybozu-go/coil/v2/runners"
+	"go.uber.org/zap/zapcore"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+const (
+	gracefulTimeout = 20 * time.Second
+)
+
+func subMain() error {
+	ctrl.SetLogger(zap.New(zap.StacktraceLevel(zapcore.DPanicLevel)))
+
+	scheme := runtime.NewScheme()
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		return err
+	}
+	if err := coilv2.AddToScheme(scheme); err != nil {
+		return err
+	}
+
+	timeout := gracefulTimeout
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                  scheme,
+		LeaderElection:          true,
+		LeaderElectionID:        "coil-leader",
+		LeaderElectionNamespace: "kube-system", // coil should run in kube-system
+		MetricsBindAddress:      config.metricsAddr,
+		GracefulShutdownTimeout: &timeout,
+		HealthProbeBindAddress:  config.healthAddr,
+	})
+	if err != nil {
+		return err
+	}
+
+	pm := ipam.NewPoolManager(mgr.GetClient(), ctrl.Log.WithName("pool-manager"), scheme)
+	apctrl := controllers.AddressPoolReconciler{
+		Client:  mgr.GetClient(),
+		Log:     ctrl.Log.WithName("addresspool-reconciler"),
+		Scheme:  scheme,
+		Manager: pm,
+	}
+	if err := apctrl.SetupWithManager(mgr); err != nil {
+		return err
+	}
+
+	brctrl := controllers.BlockRequestReconciler{
+		Client:  mgr.GetClient(),
+		Log:     ctrl.Log.WithName("blockrequest-reconciler"),
+		Scheme:  scheme,
+		Manager: pm,
+	}
+	if err := brctrl.SetupWithManager(mgr); err != nil {
+		return err
+	}
+
+	// TODO: Egress controller is not fully implemented
+	egressctrl := controllers.EgressReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("egress-reconciler"),
+		Scheme: scheme,
+	}
+	if err := egressctrl.SetupWithManager(mgr); err != nil {
+		return err
+	}
+
+	gc := &runners.GarbageCollector{
+		Client:    mgr.GetClient(),
+		APIReader: mgr.GetAPIReader(),
+		Log:       ctrl.Log.WithName("gc"),
+		Scheme:    scheme,
+		Interval:  config.gcInterval,
+	}
+	if err := mgr.Add(gc); err != nil {
+		return err
+	}
+
+	log := ctrl.Log.WithName("main")
+	log.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		log.Error(err, "problem running manager")
+		return err
+	}
+
+	return nil
+}
