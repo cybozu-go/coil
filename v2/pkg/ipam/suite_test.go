8098f57b7d41d06ece1d28df23dd74b1fd7e6cbe
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-06-11 09:56:58 +0900

    Add ErrorIfCRDPathMissing to envtest.Environment
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- v2/pkg/ipam/suite_test.go
+++ v2/pkg/ipam/suite_test.go
@@ -66,7 +66,8 @@ var _ = BeforeSuite(func() {
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
 	}
 
 	var err error
