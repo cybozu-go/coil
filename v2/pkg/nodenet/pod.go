b351021153f611a57c7eb1c86b639e5e231955aa
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2021-09-16 09:22:38 +0900

    Remove the link if findLinkLocalIPv6 failed
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- v2/pkg/nodenet/pod.go
+++ v2/pkg/nodenet/pod.go
@@ -266,6 +266,7 @@ func (pn *podNetwork) Setup(nsPath, podName, podNS string, conf *PodNetConf, hoo
 
 			containerIPv6, err = findLinkLocalIPv6(cLink)
 			if err != nil {
+				netlink.LinkDel(cLink)
 				return err
 			}
 		}
