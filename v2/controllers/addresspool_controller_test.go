b0093a06df87adf15766fe3f95a4eec59729bb1b
Tomoki Sugiura <tomoki-sugiura@cybozu.co.jp>
2025-02-07 16:02:22 +0900

    Update for k8s 1.31
    Signed-off-by: Tomoki Sugiura <tomoki-sugiura@cybozu.co.jp>

--- v2/controllers/addresspool_controller_test.go
+++ v2/controllers/addresspool_controller_test.go
@@ -10,8 +10,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/config"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 )
@@ -29,6 +31,9 @@ var _ = Describe("AddressPool reconciler", func() {
 			LeaderElection: false,
 			Metrics: metricsserver.Options{
 				BindAddress: "0",
+			},
+			Controller: config.Controller{
+				SkipNameValidation: ptr.To(true),
 			},
 		})
 		Expect(err).ToNot(HaveOccurred())
--- v2/controllers/blockrequest_watcher_test.go
+++ v2/controllers/blockrequest_watcher_test.go
@@ -9,7 +9,9 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/config"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 )
 
@@ -26,6 +28,9 @@ var _ = Describe("BlockRequest watcher", func() {
 			LeaderElection: false,
 			Metrics: metricsserver.Options{
 				BindAddress: "0",
+			},
+			Controller: config.Controller{
+				SkipNameValidation: ptr.To(true),
 			},
 		})
 		Expect(err).ToNot(HaveOccurred())
