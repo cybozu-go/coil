7f8c333d044069406b9bc46f75f9b987fddf905e
Tomoya Terashima <49265363+terassyi@users.noreply.github.com>
2026-05-12 13:07:33 +0900

    Support k8s 1.35 (#372)
    * Support k8s 1.35
    
    Signed-off-by: terashima <tomoya-terashima@cybozu.co.jp>
    
    * Update controller-runtime related codes
    
    Signed-off-by: terashima <tomoya-terashima@cybozu.co.jp>
    
    ---------
    
    Signed-off-by: terashima <tomoya-terashima@cybozu.co.jp>

--- v2/api/v2/egress_webhook.go
+++ v2/api/v2/egress_webhook.go
@@ -2,42 +2,33 @@ package v2
 
 import (
 	"context"
-	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // SetupWebhookWithManager setups the webhook for Egress
 func (r *Egress) SetupWebhookWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewWebhookManagedBy(mgr).
-		For(r).
+	return ctrl.NewWebhookManagedBy(mgr, r).
 		WithDefaulter(&EgressCustomDefaulter{}).
 		WithValidator(&EgressCustomValidator{}).
 		Complete()
 }
 
-// EgressCustomDefaulter is an empty struct that implements webhook.CustomDefaulter
+// EgressCustomDefaulter is an empty struct that implements webhook.Defaulter
 type EgressCustomDefaulter struct{}
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 // +kubebuilder:webhook:path=/mutate-coil-cybozu-com-v2-egress,mutating=true,failurePolicy=fail,sideEffects=None,groups=coil.cybozu.com,resources=egresses,verbs=create,versions=v2,name=megress.kb.io,admissionReviewVersions={v1,v1beta1}
 
-var _ webhook.CustomDefaulter = &EgressCustomDefaulter{}
+var _ admission.Defaulter[*Egress] = &EgressCustomDefaulter{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
-func (r *EgressCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
-	egress, ok := obj.(*Egress)
-	if !ok {
-		return fmt.Errorf("expected an Egress object but got a %T", obj)
-	}
-
+func (r *EgressCustomDefaulter) Default(ctx context.Context, egress *Egress) error {
 	tmpl := egress.Spec.Template
 	if tmpl == nil {
 		return nil
@@ -53,20 +44,15 @@ func (r *EgressCustomDefaulter) Default(ctx context.Context, obj runtime.Object)
 	return nil
 }
 
-// EgressCustomValidator is an empty struct that implements webhook.CustomValidator
+// EgressCustomValidator is an empty struct that implements webhook.Validator
 type EgressCustomValidator struct{}
 
 // +kubebuilder:webhook:path=/validate-coil-cybozu-com-v2-egress,mutating=false,failurePolicy=fail,sideEffects=None,groups=coil.cybozu.com,resources=egresses,verbs=create;update,versions=v2,name=vegress.kb.io,admissionReviewVersions={v1,v1beta1}
 
-var _ webhook.CustomValidator = &EgressCustomValidator{}
+var _ admission.Validator[*Egress] = &EgressCustomValidator{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *EgressCustomValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
-	egress, ok := obj.(*Egress)
-	if !ok {
-		return nil, fmt.Errorf("expected an Egress object but got a %T", obj)
-	}
-
+func (r *EgressCustomValidator) ValidateCreate(ctx context.Context, egress *Egress) (warnings admission.Warnings, err error) {
 	if errs := egress.Spec.validate(); len(errs) != 0 {
 		return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "Egress"}, egress.Name, errs)
 	}
@@ -75,20 +61,15 @@ func (r *EgressCustomValidator) ValidateCreate(ctx context.Context, obj runtime.
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *EgressCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (warnings admission.Warnings, err error) {
-	egress, ok := newObj.(*Egress)
-	if !ok {
-		return nil, fmt.Errorf("expected an Egress object but got a %T", newObj)
-	}
-
-	if errs := egress.Spec.validateUpdate(); len(errs) != 0 {
-		return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "Egress"}, egress.Name, errs)
+func (r *EgressCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj *Egress) (warnings admission.Warnings, err error) {
+	if errs := newObj.Spec.validateUpdate(); len(errs) != 0 {
+		return nil, apierrors.NewInvalid(schema.GroupKind{Group: GroupVersion.Group, Kind: "Egress"}, newObj.Name, errs)
 	}
 
 	return nil, nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *EgressCustomValidator) ValidateDelete(ctx context.Context, old runtime.Object) (warnings admission.Warnings, err error) {
+func (r *EgressCustomValidator) ValidateDelete(ctx context.Context, old *Egress) (warnings admission.Warnings, err error) {
 	return nil, nil
 }
--- v2/go.mod
+++ v2/go.mod
@@ -1,6 +1,6 @@
 module github.com/cybozu-go/coil/v2
 
-go 1.26.1
+go 1.26.3
 
 require (
 	github.com/bits-and-blooms/bitset v1.24.4
@@ -27,29 +27,27 @@ require (
 	golang.org/x/sys v0.42.0
 	google.golang.org/grpc v1.79.3
 	google.golang.org/protobuf v1.36.11
-	k8s.io/api v0.34.4
-	k8s.io/apimachinery v0.34.4
-	k8s.io/client-go v0.34.4
+	k8s.io/api v0.35.4
+	k8s.io/apimachinery v0.35.4
+	k8s.io/client-go v0.35.4
 	k8s.io/klog/v2 v2.130.1
-	k8s.io/utils v0.0.0-20250604170112-4c0f3b243397
-	sigs.k8s.io/controller-runtime v0.21.0
+	k8s.io/utils v0.0.0-20251002143259-bc988d571ff4
+	sigs.k8s.io/controller-runtime v0.23.3
 )
 
 require (
 	github.com/Masterminds/semver/v3 v3.4.0 // indirect
-	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.3 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/mod v0.32.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.13.0 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
-	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v6 v6.3.2-0.20260122202528-d9cc6641c482 // indirect
 )
 
 require (
@@ -65,7 +63,6 @@ require (
 	github.com/go-openapi/swag v0.23.1 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/gnostic-models v0.7.0 // indirect
 	github.com/google/pprof v0.0.0-20260115054156-294ebfa9ad83 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -102,9 +99,9 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/apiextensions-apiserver v0.34.3 // indirect
-	k8s.io/kube-openapi v0.0.0-20250710124328-f3f2b991d03b // indirect
-	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
+	k8s.io/apiextensions-apiserver v0.35.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20250910181357-589584f1c912 // indirect
+	sigs.k8s.io/json v0.0.0-20250730193827-2d320260d730 // indirect
 	sigs.k8s.io/knftables v0.0.18 // indirect
 	sigs.k8s.io/yaml v1.6.0 // indirect
 )
